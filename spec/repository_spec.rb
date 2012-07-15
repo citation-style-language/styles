c280a57c9f6bc081512ece1c05b0a813d55792de
Sylvester Keil <sylvester@keil.or.at>
2012-07-15 21:58:24 +0200

    fixed repository tests

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,15 +1,14 @@
 describe "The CSL Style Repository" do
 
   it "contains independent styles" do
-    IndependentStyles.should_not be_empty
+    Independents.should_not be_empty
   end
 
   it "contains dependent styles" do
-    DependentStyles.should_not be_empty
+    Dependents.should_not be_empty
   end
 
   it "does not contain any duplicate file names" do
-    (IndependentStyles + DependentStyles).map { |s| File.basename(s) }.uniq.length.should ==
-      (IndependentStyles.length + DependentStyles.length)
+    (Independents.keys & Dependents.keys).should be_empty
   end
 end
